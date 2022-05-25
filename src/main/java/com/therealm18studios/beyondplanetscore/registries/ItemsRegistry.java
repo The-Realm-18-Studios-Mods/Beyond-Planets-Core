a3eeed1d0348d8c746189ce38a7c3f70eca901fb
TheRealM18 <git@therealm18.com>
2022-05-25 11:52:03 -0300

    added ingots and changed from raw_type_ore to raw_ore_type

--- src/main/java/com/therealm18studios/beyondplanetscore/registries/ItemsRegistry.java
+++ src/main/java/com/therealm18studios/beyondplanetscore/registries/ItemsRegistry.java
@@ -19,17 +19,17 @@ public class ItemsRegistry {
 //    public static final RegistryObject<BlockItem> STAIR1ITEM = ITEMS.register("stair1", () -> new BlockItem(BlocksRegistry.STAIR1.get(), new Item.Properties()));
 
     //Raw Ores
-    public static final RegistryObject<Item> RAW_LEAD_ITEM = ITEMS.register("raw_lead_ore", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
-    public static final RegistryObject<Item> RAW_TIN_ITEM = ITEMS.register("raw_tin_ore", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
-    public static final RegistryObject<Item> RAW_URANIUM_ITEM = ITEMS.register("raw_uranium_ore", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
-    public static final RegistryObject<Item> RAW_ZINC_ITEM = ITEMS.register("raw_zinc_ore", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
+    public static final RegistryObject<Item> RAW_LEAD_ITEM = ITEMS.register("raw_ore_lead", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
+    public static final RegistryObject<Item> RAW_TIN_ITEM = ITEMS.register("raw_ore_tin", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
+    public static final RegistryObject<Item> RAW_URANIUM_ITEM = ITEMS.register("raw_ore_uranium", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
+    public static final RegistryObject<Item> RAW_ZINC_ITEM = ITEMS.register("raw_ore_zinc", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
 
 
     //Ingots
-    public static final RegistryObject<Item> LEAD_INGOT = ITEMS.register("lead_ingot", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
-    public static final RegistryObject<Item> TIN_INGOT = ITEMS.register("tin_ingot", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
-    public static final RegistryObject<Item> URANIUM_INGOT = ITEMS.register("uranium_ingot", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
-    public static final RegistryObject<Item> ZINC_INOGT = ITEMS.register("zinc_ingot", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
+    public static final RegistryObject<Item> LEAD_INGOT = ITEMS.register("ingot_lead", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
+    public static final RegistryObject<Item> TIN_INGOT = ITEMS.register("ingot_tin", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
+    public static final RegistryObject<Item> URANIUM_INGOT = ITEMS.register("ingot_uranium", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
+    public static final RegistryObject<Item> ZINC_INOGT = ITEMS.register("ingot_zinc", () -> new Item(new Item.Properties().tab(ItemGroups.TAB_ITEMS)));
 
     //Mars Ores
     public static final RegistryObject<BlockItem> MARS_COPPER_ORE_ITEM = ITEMS.register("mars_copper_ore", () -> new BlockItem(BlocksRegistry.MARS_COPPER_ORE.get(), new Item.Properties().tab(ItemGroups.TAB_ORES)));
